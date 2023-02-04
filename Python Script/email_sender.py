5638e6dd98702f3f4b79fbfac14be9c39bdd7269
Dylan Topolovec <dctopolovec2@gmail.com>
2023-02-04 13:27:08 -0500

    Started python project

---
+++ Python Script/email_sender.py
@@ -0,0 +1,23 @@
+# Used to send email
+import smtplib
+
+# Used to construct email message contents
+import email
+
+# Email contents filename
+filename = 'test_file.txt'
+
+# Open and read file, then process it to send as an email
+with open(filename) as fp:
+    msg = email.message.EmailMessage()
+    msg.set_content(fp.read())
+
+# Email info
+msg['Subject'] = f'The contents of {filename}'
+msg['From'] = 'BinghamtonInternships@gmail.com'
+msg['To'] = 'BinghamtonInternships@gmail.com'
+
+# Send the message
+s = smtplib.SMTP('localhost')
+s.send_message(msg)
+s.quit()
